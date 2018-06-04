5677045ee42cd4f7c67d4a708df82b0b4817c3a1
Maximo DeLaRosa <mjd809@gmail.com>
2018-06-04 18:43:29 -0400

    Graph initial commit

--- projects/graph/src/App.js
+++ projects/graph/src/App.js
@@ -3,8 +3,8 @@ import { Graph } from './graph';
 import './App.css';
 
 // !!! IMPLEMENT ME
-// const canvasWidth = 
-// const canvasHeight = 
+const canvasWidth = 700;
+const canvasHeight = 1000;
 
 /**
  * GraphView
@@ -32,14 +32,47 @@ class GraphView extends Component {
     let ctx = canvas.getContext('2d');
     
     // Clear it
-    ctx.fillStyle = 'white';
-    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    // if you give this a color you don't recognize it will turn black 
+    // it won't give us appropriate error;
+    // ctx.fillStyle = 'teal';
+    // ctx.fillRect(200, 300, canvasWidth, canvasHeight);
+    ctx.fillStyle = 'blue';    
+    ctx.fillRect(200, 150, canvasWidth, canvasHeight);
+    ctx.fillStyle = 'rgb(0, 206, 0';    
+    ctx.fillRect(50, 50, 50, 50);
+    ctx.fillStyle = 'rgb(209, 206, 0)';    
+    ctx.fillRect(250, 250, 25, 25);
 
+    ctx.moveTo(10,20);
+    ctx.lineTo(1300,900);
+    ctx.stroke();
+
+    ctx.lineTo(150,160);
+    ctx.stroke();
+
+    ctx.beginPath(); // begin path closes the circle
+    // ctx.arc(95,50,40,0,2*Math.PI);
+    // ctx.moveTo(500, 50);
+    ctx.arc(500,50,10,0,3*Math.PI);
+    ctx.moveTo(10, 50);
+    ctx.arc(300,50,10,0,3*Math.PI);
+    ctx.moveTo(10, 10);
+    // ctx.moveTo(100, 50);
+    // ctx.arc(180,50,10,0,2*Math.PI);
+    // ctx.arc(180,50,10,0,2*Math.PI);
+    ctx.stroke();
+    for(let i = 0; i < 500; i++) {
+      ctx.beginPath();
+      ctx.moveTo(0, 0);
+      ctx.lineTo(i * 10, 600);
+      ctx.stroke();
+    }
     // !!! IMPLEMENT ME
     // compute connected components
     // draw edges
     // draw verts
     // draw vert values (labels)
+    
   }
   
   /**
